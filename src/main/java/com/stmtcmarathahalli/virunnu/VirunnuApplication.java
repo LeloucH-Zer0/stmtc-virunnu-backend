7601ce5576cfc1f6cbda0a24b32ef889e3f5686a
Justine Thomas <justine.thomas@onedirect.in>
2019-07-31 19:38:17 +0530

    Adding base setup to master

---
+++ src/main/java/com/stmtcmarathahalli/virunnu/VirunnuApplication.java
@@ -0,0 +1,13 @@
+package com.stmtcmarathahalli.virunnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirunnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirunnuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stmtcmarathahalli/virunnu/VirunnuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stmtcmarathahalli.virunnu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirunnuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
